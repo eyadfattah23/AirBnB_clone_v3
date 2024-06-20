cdee5200f744c8f5fe4f03044f45fee794cf503b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-20 11:56:46 +0300

    update edit readme style edit base model tests to fix time problem add test for new and all method in dbstorage  create some data for testing  remove unnecessary lines in amenities tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -78,11 +78,26 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
+        new_obj = Amenity(name='jajanken')
+        models.storage.new(new_obj)
+        self.assertIn(f'Amenity.{new_obj.id}', models.storage.all())
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_new(self):
         """test that new adds an object to the database"""
+        new_obj = Amenity(name='jajanken')
+        models.storage.new(new_obj)
+        self.assertIn(f'Amenity.{new_obj.id}', models.storage.all(Amenity))
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_save(self):
-        """Test that save properly saves objects to file.json"""
+        init_len = len(models.storage.all())
+        new_obj = Amenity(name='California')
+        new_obj2 = Amenity(name='California123')
+        models.storage.new(new_obj)
+        models.storage.new(new_obj2)
+
+        self.assertEqual(init_len + 2, len(models.storage.all()))
+
+        self.assertIn(f'{new_obj.__class__.__name__}.{new_obj.id}',
+                      models.storage.all().keys())
+
+        self.assertIn(new_obj, models.storage.all().values())
