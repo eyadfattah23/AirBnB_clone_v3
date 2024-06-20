54e9b70d52e7ee2b524919d602c8c3d0ea84218b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-20 13:42:30 +0300

    add count and get methods in db and file storages

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,33 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieve one object
+
+        Args:
+            cls (class): type of object (state, city, place, .... etc)
+            id (str): string representing the object ID
+            Returns the object based on the class and its ID,
+                    or None if not found
+        """
+        if cls and id:
+            for obj in self.all(cls).values():
+                if id == obj.id:
+                    return obj
+            return None
+        return None
+
+    def count(self, cls=None):
+        """ count the number of objects in storage
+
+           Returns the number of objects in storage matching the given class.
+           If no class is passed, returns the count of all objects in storage.
+
+           Args:
+               cls (class, optional): _description_. Defaults to None.
+           """
+        if cls:
+            return len(self.all(cls))
+
+        return len(self.all())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
@@ -55,7 +54,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as e:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +67,32 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object
+
+        Args:
+            cls (class): type of object (state, city, place, .... etc)
+            id (str): string representing the object ID
+            Returns the object based on the class and its ID,
+                or None if not found
+        """
+        if (cls) and (id) and (cls in classes.values()) and type(id) is str:
+            for obj in self.all(cls).values():
+                if id == obj.id:
+                    return obj
+        return None
+
+    def count(self, cls=None):
+        """ count the number of objects in storage
+
+           Returns the number of objects in storage matching the given class.
+           If no class is passed, returns the count of all objects in storage.
+
+           Args:
+               cls (class, optional): _description_. Defaults to None.
+           """
+        if cls and (cls in classes.values()):
+            return len(self.all(cls))
+
+        return len(self.all())
