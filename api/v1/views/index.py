6a27c388dceebc35d2ea91b07243c5bb24e93883
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-21 16:43:42 +0300

    add stats endpoint and it's tests

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 '''a module as an index'''
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -10,3 +17,18 @@ def status():
     when /status is visited
     OK if running"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    '''retrieves the number of each object by type:'''
+
+    dictionary = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(dictionary)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,13 @@
 import unittest
 from flask import Flask
 from api.v1.app import app
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class StatusEndpointTestCase(unittest.TestCase):
@@ -17,3 +24,22 @@ class StatusEndpointTestCase(unittest.TestCase):
         response = self.client.get('/api/v1/status')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {'status': 'OK'})
+
+    def test_stats_endpoint_(self):
+        """Test the /stats endpoint."""
+        response = self.client.get('/api/v1/stats')
+        amenities = storage.count(Amenity)
+        cities = storage.count(City)
+        places = storage.count(Place)
+        reviews = storage.count(Review)
+        states = storage.count(State)
+        users = storage.count(User)
+        stats = {
+            "amenities": amenities,
+            "cities": cities,
+            "places": places,
+            "reviews": reviews,
+            "states": states,
+            "users": users
+        }
+        self.assertDictEqual(response.json, stats)
