2248d31a32a27d1d6b39dd2a0415a93222db4562
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-06-25 19:31:46 +0300

    try to solve the doc issue

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""view for Place objects
+"""view for all Place objects
 that handles all default RESTFul API actions"""
 
-from models import storage
 from models.city import City
 from models.place import Place
+from models import storage
 from models.user import User
 
 from flask import abort, jsonify, request
